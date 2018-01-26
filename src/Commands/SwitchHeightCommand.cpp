f9be6859dbff1025c5c022900cf06c1d9f92ddec
Unknown <haywirerobotics1569@gmail.com>
2018-01-25 20:33:12 -0700

    The SwitchCommand now works in tandem with the teleopCommand
    Okay so, finally the command works properly.  Don't expect to not touch this code ever again.  We will have to do so.  Also all computers hate Cade, becuase he doesn't have the right touch.

--- src/Commands/SwitchHeightCommand.cpp
+++ src/Commands/SwitchHeightCommand.cpp
@@ -1,5 +1,6 @@
 #include "SwitchHeightCommand.h"
 #include "../Robot.h"
+#include <iostream>
 
 SwitchHeightCommand::SwitchHeightCommand() {
 	// Use Requires() here to declare subsystem dependencies
@@ -10,13 +11,14 @@ SwitchHeightCommand::SwitchHeightCommand() {
 // Called just before this Command runs the first time
 void SwitchHeightCommand::Initialize() {
 	Robot::elevatorSubsystem.get()->InitCounter();
+	Robot::elevatorSubsystem.get()->movingToPosition = true;
 
 //	Robot::elevatorSubsystem.get()->ElevateUp(0.5);
 }
 
 // Called repeatedly when this Command is scheduled to run
 void SwitchHeightCommand::Execute() {
-	Robot::elevatorSubsystem.get()->ElevateUp(0.5);
+	Robot::elevatorSubsystem.get()->ElevatorSet(0.1);
 }
 
 // Make this return true when this Command no longer needs to run execute()
@@ -27,6 +29,7 @@ bool SwitchHeightCommand::IsFinished() {
 // Called once after isFinished returns true
 void SwitchHeightCommand::End() {
 	Robot::elevatorSubsystem.get()->StopElevate();
+	Robot::elevatorSubsystem.get()->movingToPosition = false;
 }
 
 // Called when another command which requires one or more of the same
