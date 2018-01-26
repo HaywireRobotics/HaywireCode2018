958946b3186c0fce36370a9d4e47a94f848beb0c
Unknown <haywirerobotics1569@gmail.com>
2018-01-25 18:54:22 -0700

    Merge remote-tracking branch 'origin/CommandBased' into CommandBased

---
+++ src/Commands/DriveForward.h
@@ -0,0 +1,22 @@
+#ifndef DriveForward_H
+#define DriveForward_H
+
+#include <Commands/Command.h>
+#include "Subsystems/DriveTrainSubsystem.h"
+#include "../Robot.h"
+#include <Timer.h>
+
+class DriveForward : public frc::Command {
+public:
+	DriveForward(double);
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+private:
+	Timer *time;
+	double seconds;
+};
+
+#endif  // DriveForward_H
