aa350856516ac76a9eb54d0bc181d2ce88ae0fdd
Unknown <haywirerobotics1569@gmail.com>
2018-02-15 20:44:43 -0700

    Added files for dropping and lifting the forklift

---
+++ src/Commands/dropLift.cpp
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "dropLift.h"
+
+dropLift::dropLift() {
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(Robot::chassis.get());
+}
+
+// Called just before this Command runs the first time
+void dropLift::Initialize() {
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void dropLift::Execute() {
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool dropLift::IsFinished() {
+	return false;
+}
+
+// Called once after isFinished returns true
+void dropLift::End() {
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void dropLift::Interrupted() {
+
+}
